4eff35ef4ed3c0c2e705574e627c951909a658c2
qirtudgus <qirtudgus@naver.com>
2022-12-22 05:50:57 +0900

    feat: 게시물목록을 db에서 가져오는 게시판 컴포넌트

---
+++ pages/posts/list.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import React from 'react';
+import styled from 'styled-components';
+
+const TT = styled.h1`
+  color: #fff;
+`;
+
+export default function BoardList(props: any) {
+  console.log(props);
+  return (
+    <>
+      <ul>
+        {props.data.map((el: any) => {
+          return (
+            <React.Fragment key={el.idx}>
+              <li>
+                <Link href={`/posts/${el.idx}`}>
+                  {el.idx} {el.nickname} {el.title} 날짜 : {el.date}
+                </Link>
+              </li>
+            </React.Fragment>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  //context 인자에는 다양한 키가 들어있다. 아래 코드는 context에서 동적 경로 페이지 정보를 가져와서 그 번호로 axios 요청을 한것
+  let { data } = await axios.get(`http://localhost:3000/api/getBoardList`);
+  return {
+    props: {
+      data,
+    },
+  };
+};
