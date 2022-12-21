c140354b619b3cf7e275ac10b5a88907740aaa76
qirtudgus <qirtudgus@naver.com>
2022-12-21 09:43:44 +0900

    feat:api 라우트 기능을 통한 SSR 테스트
    전송하는 데이터는 클라이언트에 있는 코드지만
    api 통신과 getServerSideProps 테스트하기 위해
    코드 변경

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { boardArr } from '../home/title';
 
-type Data = {
-  name: string
+interface Data {
+  index: number;
+  title: string;
+  date: string;
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log('리퀘스트 쿼리 결과');
+  console.log(req.query);
+  res.status(200).json(boardArr[Number(req.query.idx) - 1]);
 }
