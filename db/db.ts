76e250c66c701f4ad5a0b689778e6fefd3f81be6
qirtudgus <qirtudgus@naver.com>
2022-12-22 00:43:14 +0900

    feat: db파일 추가

---
+++ db/db.ts
@@ -0,0 +1,28 @@
+import mysql from 'serverless-mysql';
+
+const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT as unknown as number,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    charset: 'utf8mb4',
+  },
+});
+
+export default async function excuteQuery({
+  query,
+  values,
+}: {
+  query: string;
+  values: any;
+}) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+}
