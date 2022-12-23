2a8e82376d622264ba618e7b0b5f212c0f0e0a1e
qirtudgus <qirtudgus@naver.com>
2022-12-23 11:45:34 +0900

    feat: CustomAxios 추가
    이전 프로젝트에선 결과값을 return해주지않았지만
    해주도록 변경

---
+++ utils/customAxios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+type AxiosMethod = 'post' | 'get' | 'put' | 'delete';
+
+// const DOMAIN_ENV = process.env.NEXT_PUBLIC_SERVER_API;
+const DOMAIN_ENV = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SERVER_API : 'http://localhost:3000';
+axios.defaults.withCredentials = true;
+export const customAxios = async (method: AxiosMethod, url: string, data?: any): Promise<any> => {
+  return await axios({
+    method,
+    url: DOMAIN_ENV + '/api' + url,
+    data,
+  })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+export default customAxios;
