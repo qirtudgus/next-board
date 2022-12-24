8aeae982b784188431c9f639cb2e79f3aafe0025
qirtudgus <qirtudgus@naver.com>
2022-12-24 17:13:02 +0900

    feat: CreateAsyncThunk 테스트를 위한 API 라우트

---
+++ pages/api/testCounterNumberApi.ts
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<number>) {
+  res.status(200).json(120112);
+}
