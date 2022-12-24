aebbe13ad251ec8234084240586484a70e63702c
qirtudgus <qirtudgus@naver.com>
2022-12-24 17:12:38 +0900

    feat: NextReduxWrapper로 app을 감쌌다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from './layout/layout';
+import { Provider } from 'react-redux';
+import wrapper from '../store/store';
+function App({ Component, pageProps }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
 
-export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
+
+export default App;
