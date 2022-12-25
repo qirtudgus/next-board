fc3a1133cf7d185f820afa95fef61fefa8acf26e
qirtudgus <qirtudgus@naver.com>
2022-12-26 01:46:26 +0900

    feat: 공용 버튼컴포넌트 생성

---
+++ components/BasicButton.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+const Button = styled.button<BasicButtonStyleInterface>`
+  width: 100%;
+  height: 48px;
+  margin-top: 25px;
+  border: none;
+  border-radius: 4px;
+  font-size: 1.1em;
+  max-width: ${(props) => props.width}px;
+  background-color: #fff;
+  border: 1px solid#dfe1e5;
+  &:hover {
+    background-color: #dfdfdf;
+  }
+`;
+
+const Solid = styled(Button)`
+  border: none;
+  background-color: ${({ theme }) => theme.colors.main};
+  color: #fff;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.mainHoverColor};
+  }
+`;
+
+interface BasicButtonStyleInterface {
+  width: number;
+}
+
+interface BasicButtonInterface {
+  BasicButtonValue: string;
+  width: number;
+  children?: React.ReactNode;
+  OnClick: () => void;
+}
+
+export function BasicButton(props: BasicButtonInterface) {
+  return (
+    <Button
+      width={props.width}
+      onClick={props.OnClick}
+    >
+      <span>{props.BasicButtonValue}</span>
+    </Button>
+  );
+}
+
+export function SolidButton(props: BasicButtonInterface) {
+  return (
+    <Solid
+      width={props.width}
+      onClick={props.OnClick}
+    >
+      <span>{props.BasicButtonValue}</span>
+    </Solid>
+  );
+}
+
+export default BasicButton;
