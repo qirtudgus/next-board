d9db9710e70201b86e28fbca8c50bf85ba4d58c3
qirtudgus <qirtudgus@naver.com>
2022-12-26 01:55:11 +0900

    feat: 아이디 중복확인 API 생성

---
+++ pages/api/join/duplicationCheckId.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import excuteQuery from '../../../db/db';
+import { DuplicationCheckIdState } from '../../../store/checkIdSlice';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<DuplicationCheckIdState>) {
+  try {
+    console.log(req.body.id);
+    await excuteQuery({
+      query: 'SELECT userId FROM user_table WHERE userId = ?',
+      values: req.body.id,
+    }).then((checkIdRespones) => {
+      if ((checkIdRespones as any[]).length === 0) {
+        console.log('사용 가능한 아이디');
+        res.status(200).json({ idValue: false, idStatusText: '사용 가능한 아이디입니다.' });
+      } else {
+        console.log('이미 사용중인 아이디');
+        res.status(200).json({ idValue: true, idStatusText: '이미 사용중인 아이디입니다.' });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
