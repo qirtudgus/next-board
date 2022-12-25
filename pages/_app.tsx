3c086b56833bf81caa04546f09d52705ed1c528c
qirtudgus <qirtudgus@naver.com>
2022-12-25 23:42:07 +0900

    feat: ThemeProvider 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,19 @@ import type { AppProps } from 'next/app';
 import Layout from './layout/layout';
 import { Provider } from 'react-redux';
 import wrapper from '../store/store';
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/GlobalStyles';
+
 function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
 
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </Provider>
   );
 }
