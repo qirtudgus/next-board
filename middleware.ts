9d71f5b0018c4d19bc011d7babc854df38469399
qirtudgus <qirtudgus@naver.com>
2022-12-26 15:12:49 +0900

    test: 미들웨어 쿠키 획득 테스트

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,17 @@ export function middleware(req: NextRequest) {
 
   // '/posts/*'경로에서 실행된다.
   if (req.nextUrl.pathname.startsWith('/posts')) {
+    //특정 쿠키를 가져올 수 있다.
+    // const cookie = req.cookies.getAll();
+    // console.log(cookie);
+    //특정 쿠키의 유무를 반환해준다. boolean
+    // const hasCookie = req.cookies.has('_ga_HSYFWJC9F7');
+    // console.log(hasCookie);
     console.log('게시물을 볼 때 미들웨어');
+
+    //미들웨어에서 쿠키 생성
+    // const response = NextResponse.next();
+    // response.cookies.set('myCookie', 'Hello~~');
+    // return response;
   }
 }
