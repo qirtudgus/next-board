a0fdd8949683c2d8fd0770ed0631d16271a19385
qirtudgus <qirtudgus@naver.com>
2022-12-28 23:24:50 +0900

    feat: 토큰값에 idx 추가

--- utils/createToken.ts
+++ utils/createToken.ts
@@ -1,29 +1,37 @@
 import { JWTPayload, jwtVerify, SignJWT } from 'jose';
 
-export const createJoseAccessToken = async (id: string): Promise<string> => {
+export const createJoseAccessToken = async (id: string, idx: string | number): Promise<string> => {
   //시크릿키 생성
   const secret = new TextEncoder().encode(process.env.SECRET_TOKEN);
   //알고리즘 선택
   const alg = process.env.JWT_ALG as string;
-  const jwt = await new SignJWT({ id }).setProtectedHeader({ alg }).setIssuedAt().setExpirationTime('1s').sign(secret);
+  const jwt = await new SignJWT({ idx, id })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime('5s')
+    .sign(secret);
 
   return jwt;
 };
 
-export const createJoseRefreshToken = async (id: string): Promise<string> => {
+export const createJoseRefreshToken = async (id: string, idx: string | number): Promise<string> => {
   //시크릿키 생성
   const secret = new TextEncoder().encode(process.env.SECRET_TOKEN);
   //알고리즘 선택
   const alg = process.env.JWT_ALG as string;
-  const jwt = await new SignJWT({ id }).setProtectedHeader({ alg }).setIssuedAt().setExpirationTime('5s').sign(secret);
+  const jwt = await new SignJWT({ idx, id })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime('10s')
+    .sign(secret);
 
   return jwt;
 };
 
-export const verifyJoseToken = async (token: string): Promise<JWTPayload | null> => {
+export const verifyJoseToken = async (token: string | undefined): Promise<JWTPayload | null> => {
   const secret = new TextEncoder().encode(process.env.SECRET_TOKEN);
   try {
-    const { payload } = await jwtVerify(token, secret);
+    const { payload } = await jwtVerify(token as string, secret);
     return payload;
   } catch (err) {
     return null;
