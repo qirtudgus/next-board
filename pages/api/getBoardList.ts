b063f56350f829d085df45c885424c92d2ab21ad
qirtudgus <qirtudgus@naver.com>
2023-01-01 05:04:34 +0900

    feat: 게시판 최신순으로 렌더링되도록 응답

--- pages/api/getBoardList.ts
+++ pages/api/getBoardList.ts
@@ -5,6 +5,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   const page = req.query.page as string;
   //쿼리스트링에 담은 현재 페이지 정보
   let pageNumber = (Number(page) - 1) * 10; // 0
+  console.log('쿼리 확인');
   console.log(req.query);
   try {
     await excuteQuery({
@@ -12,7 +13,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
       values: '',
     }).then((queryResult) => {
       //10개 자른 게시판 데이터
-      let list = (queryResult as any[]).slice(pageNumber, pageNumber + 9 + 1);
+      let list = (queryResult as any[]).reverse().slice(pageNumber, pageNumber + 9 + 1);
       //총 게시물을 보여주는데 필요한 페이지 수
       let listNum = Math.ceil((queryResult as any[]).length / 10);
       res.status(200).send({ list, listNum });
