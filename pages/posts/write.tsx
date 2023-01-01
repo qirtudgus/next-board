f398e2618d397c9cc7f8b9a3547e5a58f9057ef6
qirtudgus <qirtudgus@naver.com>
2023-01-02 04:36:50 +0900

    feat: 작성 시 userId 데이터추가

--- pages/posts/write.tsx
+++ pages/posts/write.tsx
@@ -2,31 +2,32 @@ import axios from 'axios';
 import moment from 'moment';
 import { useRouter } from 'next/router';
 import { RefObject, useRef, useState } from 'react';
+import { useAppSelector } from '../../store/store';
 
 const Write = () => {
   let nameRef = useRef() as RefObject<HTMLInputElement>;
   let titleRef = useRef() as RefObject<HTMLInputElement>;
   let contentRef = useRef() as RefObject<HTMLInputElement>;
+  const userId = useAppSelector((state) => state.userInfoSlice.id);
   const router = useRouter();
 
   return (
     <>
-      닉네임 : <input ref={nameRef}></input>
       제목 : <input ref={titleRef}></input>
       내용 : <input ref={contentRef}></input>
       <button
         onClick={() => {
           console.log(nameRef.current?.value);
           axios
-            .post('/api/addBoard', {
-              nickname: nameRef.current?.value,
+            .post('/api/posts', {
+              userId,
               title: titleRef.current?.value,
               content: contentRef.current?.value,
               date: moment().format('YYYY-MM-DD hh:mm:ss'),
             })
             .then((res) => {
               if (res.status === 201) {
-                router.push('/posts/list');
+                router.push('/board');
               }
             });
         }}
