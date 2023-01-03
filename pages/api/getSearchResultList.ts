e105078e7d3bd46724512f49185418469615edf3
qirtudgus <qirtudgus@naver.com>
2023-01-03 13:19:21 +0900

    ing: 필터링과 페이징을 동시에 하려는중...

--- pages/api/getSearchResultList.ts
+++ pages/api/getSearchResultList.ts
@@ -3,20 +3,36 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   const page = req.query.page as string;
+  //자동으로 디코딩되는듯..
   const keyword = req.query.keyword as string;
+  const sort = req.query.sort as string;
   const theme = (req.query.theme as string) === '제목' ? 'title' : 'userId';
   let pageNumber = (Number(page) - 1) * 10; // 0
 
-  try {
-    await excuteQuery({
-      query: `SELECT * FROM board_table WHERE ${theme} LIKE ?`,
-      values: ['%' + keyword + '%'],
-    }).then((queryResult) => {
-      let list = (queryResult as any[]).slice(pageNumber, pageNumber + 9 + 1);
-      let listNum = Math.ceil((queryResult as any[]).length / 10);
+  console.log('소트들어온거 확인, 없으면 undifined');
+  console.log(req.query);
 
-      res.status(200).send({ list, listNum });
-    });
+  try {
+    if (sort) {
+      console.log('뭐로 정렬할래');
+      await excuteQuery({
+        query: `SELECT * FROM board_table WHERE ${theme} LIKE ?`,
+        values: ['%' + keyword + '%'],
+      }).then((queryResult) => {
+        let list = (queryResult as any[]).reverse().slice(pageNumber, pageNumber + 9 + 1);
+        let listNum = Math.ceil((queryResult as any[]).length / 10);
+        res.status(200).send({ list, listNum });
+      });
+    } else {
+      await excuteQuery({
+        query: `SELECT * FROM board_table WHERE ${theme} LIKE ?`,
+        values: ['%' + keyword + '%'],
+      }).then((queryResult) => {
+        let list = (queryResult as any[]).slice(pageNumber, pageNumber + 9 + 1);
+        let listNum = Math.ceil((queryResult as any[]).length / 10);
+        res.status(200).send({ list, listNum });
+      });
+    }
   } catch (err) {
     console.log(err);
   }
