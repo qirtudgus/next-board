1e93e055821bb5bd832add26bc4a6a7a48f5ca68
qirtudgus <qirtudgus@naver.com>
2023-01-05 12:23:00 +0900

    feat: 게시물 좋아요 api 추가

---
+++ pages/api/postlike.ts
@@ -0,0 +1,43 @@
+import excuteQuery from '../../db/db';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { userIdx, boardIdx, behavior },
+    method,
+  } = req;
+  let likeQuery =
+    'UPDATE board_table SET likeCount = likeCount +1, likeList = CONCAT_WS("",likeList, ? ",") WHERE idx =   ?  ';
+  let unlikeQuery =
+    'UPDATE board_table SET likeCount = likeCount -1, likeList = replace(likeList, ? , "" ) WHERE idx =   ?  ';
+
+  const userIdx2 = userIdx + ',';
+
+  switch (method) {
+    case 'PUT':
+      try {
+        if (behavior === 'like') {
+          console.log(`${userIdx}님이 ${boardIdx}글을 추천하였습니다.`);
+          await excuteQuery({
+            query: likeQuery,
+            values: [userIdx, boardIdx],
+          }).then(() => {
+            res.status(200).end();
+          });
+        } else {
+          console.log(`${userIdx}님이 ${boardIdx}글을 추천 취소하였습니다.`);
+          await excuteQuery({
+            query: unlikeQuery,
+            values: [userIdx2, boardIdx],
+          }).then(() => {
+            res.status(200).end();
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+      break;
+  }
+};
+
+export default handler;
