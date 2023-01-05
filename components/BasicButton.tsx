963a3c5205c8b02ef41b0ab6024108f924c058ea
qirtudgus <qirtudgus@naver.com>
2023-01-05 12:18:52 +0900

    style : margin-top과 height값을 props로 전달받도록 추가

--- components/BasicButton.tsx
+++ components/BasicButton.tsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 const Button = styled.button<BasicButtonStyleInterface>`
   width: 100%;
-  height: 48px;
-  margin-top: 25px;
+  /* height: 48px; */
+  height: ${(props) => props.height}px;
+  /* margin-top: 25px; */
   border: none;
   border-radius: 4px;
   font-size: 1.1em;
   max-width: ${(props) => props.width}px;
+  margin-top: ${(props) => props.marginTop}px;
   background-color: #fff;
 
   border: 1px solid#dfe1e5;
@@ -29,11 +31,15 @@ const Solid = styled(Button)`
 
 interface BasicButtonStyleInterface {
   width: number;
+  height: number;
+  marginTop: number;
 }
 
 interface BasicButtonInterface {
   BasicButtonValue: string;
   width: number;
+  height: number;
+  marginTop: number;
   children?: React.ReactNode;
   OnClick: () => void;
 }
@@ -41,6 +47,8 @@ interface BasicButtonInterface {
 export function BasicButton(props: BasicButtonInterface) {
   return (
     <Button
+      height={props.height}
+      marginTop={props.marginTop}
       width={props.width}
       onClick={props.OnClick}
     >
@@ -52,6 +60,8 @@ export function BasicButton(props: BasicButtonInterface) {
 export function SolidButton(props: BasicButtonInterface) {
   return (
     <Solid
+      height={props.height}
+      marginTop={props.marginTop}
       width={props.width}
       onClick={props.OnClick}
     >
@@ -62,6 +72,8 @@ export function SolidButton(props: BasicButtonInterface) {
 
 SolidButton.defaultProps = {
   width: 100,
+  height: 48,
+  marginTop: 25,
 };
 
 export default BasicButton;
