5b07db6dd4ce8684dd0c1b602dbe63817b2fd393
qirtudgus <qirtudgus@naver.com>
2023-01-06 22:16:26 +0900

    feat: 토큰 생성 환경변수 주소 수정

--- utils/createToken.ts
+++ utils/createToken.ts
@@ -2,7 +2,7 @@ import { JWTPayload, jwtVerify, SignJWT } from 'jose';
 
 export const createJoseAccessToken = async (id: string, idx: string | number): Promise<string> => {
   //시크릿키 생성
-  const secret = new TextEncoder().encode(process.env.SECRET_TOKEN);
+  const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_SECRET_TOKEN);
   //알고리즘 선택
   const alg = process.env.JWT_ALG as string;
   const jwt = await new SignJWT({ idx, id })
@@ -16,7 +16,7 @@ export const createJoseAccessToken = async (id: string, idx: string | number): P
 
 export const createJoseRefreshToken = async (id: string, idx: string | number): Promise<string> => {
   //시크릿키 생성
-  const secret = new TextEncoder().encode(process.env.SECRET_TOKEN);
+  const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_SECRET_TOKEN);
   //알고리즘 선택
   const alg = process.env.JWT_ALG as string;
   const jwt = await new SignJWT({ idx, id })
@@ -29,7 +29,7 @@ export const createJoseRefreshToken = async (id: string, idx: string | number): 
 };
 
 export const verifyJoseToken = async (token: string | undefined): Promise<JWTPayload | null> => {
-  const secret = new TextEncoder().encode(process.env.SECRET_TOKEN);
+  const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_SECRET_TOKEN);
   try {
     const { payload } = await jwtVerify(token as string, secret);
     return payload;
