8903efd5b5bbed619175c6e8aa2fdfbab4fcbdb2
qirtudgus <qirtudgus@naver.com>
2023-01-06 11:32:56 +0900

    오리진에 도메인 값 변경

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ module.exports = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_SERVER_API },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
