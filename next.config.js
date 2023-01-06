7da06d90e968365610824cd95d883a9d12f875cc
qirtudgus <qirtudgus@naver.com>
2023-01-06 22:44:01 +0900

    fix: 헤더 주석처리

--- next.config.js
+++ next.config.js
@@ -4,17 +4,17 @@ const nextConfig = {
   compiler: {
     styledComponents: { displayName: true, ssr: true, minify: true },
   },
-  source: '/(.*)',
-  headers: [
-    { key: 'Access-Control-Allow-Credentials', value: 'true' },
-    { key: 'Access-Control-Allow-Origin', value: '*' },
-    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-    {
-      key: 'Access-Control-Allow-Headers',
-      value:
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    },
-  ],
+  // source: '/(.*)',
+  // headers: [
+  //   { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //   { key: 'Access-Control-Allow-Origin', value: '*' },
+  //   { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+  //   {
+  //     key: 'Access-Control-Allow-Headers',
+  //     value:
+  //       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  //   },
+  // ],
 };
 module.exports = nextConfig;
 
