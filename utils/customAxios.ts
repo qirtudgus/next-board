e0ac7935008e612efbfc2a5cb993207ec6fd718b
qirtudgus <qirtudgus@naver.com>
2023-01-06 11:03:20 +0900

    test: CORS 테스트

--- utils/customAxios.ts
+++ utils/customAxios.ts
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 type AxiosMethod = 'POST' | 'GET' | 'PUT' | 'DELETE';
 
-const DOMAIN_ENV = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SERVER_API : 'http://localhost:3000';
+const DOMAIN_ENV =
+  process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SERVER_API : 'http://localhost:3000/';
 axios.defaults.withCredentials = true;
 export const customAxios = async (method: AxiosMethod, url: string, data?: any): Promise<any> => {
   return await axios({
     method,
-    url: DOMAIN_ENV + '/api' + url,
+    url: DOMAIN_ENV + 'api' + url,
     data,
   })
     .then((res) => {
