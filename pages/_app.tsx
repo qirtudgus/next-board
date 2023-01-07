92d8d7684a1cf7792336c31bbbf37192f0ba8662
qirtudgus <qirtudgus@naver.com>
2023-01-07 10:55:21 +0900

    feat: 새로고침 시 레이아웃이 깜빡거리던 현상 수정
    isLoadUser가 true가 되는 시점에 메인컴포넌트만 렌더링하던 부분을
    레이아웃까지 감싸서 렌더링시키는것으로 해결
    
    setIsLoadUser 호출시점은 디스패치 코드블럭이 끝나고 난 뒤로 옮겨서
    한줄로 줄였다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,17 +24,19 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
   useEffect(() => {
     const loadUser = async () => {
       //해당주소는 클라이언트의 쿠키를 검증한 뒤 아이디와 인덱스를 반환해준다.
-      await customAxios('GET', '/loadUser').then((res) => {
-        if (res.status === 200) {
-          let id = res.data.id;
-          let idx = res.data.idx;
-          store.dispatch(loginSuccess({ id, idx }));
+      await customAxios('GET', '/loadUser')
+        .then((res) => {
+          if (res.status === 200) {
+            let id = res.data.id;
+            let idx = res.data.idx;
+            store.dispatch(loginSuccess({ id, idx }));
+          } else {
+            store.dispatch(logoutSuccess());
+          }
+        })
+        .then(() => {
           setIsLoadUser(true);
-        } else {
-          store.dispatch(logoutSuccess());
-          setIsLoadUser(true);
-        }
-      });
+        });
     };
     loadUser();
   }, []);
@@ -42,10 +44,12 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
   return getLayout(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <Layout>
-          {/* isLoading이 true가 되고나면 페이지를 로드시켜준다, 이걸로 새로고침되었을 때에도 게시판에서 초기값이 0이라 좋아요 리스트가 무조건 좋아요로 나오던 오류가 수정된다. */}
-          {isLoadUser && <Component {...pageProps} />}
-        </Layout>
+        {/* isLoading이 true가 되고나면 페이지를 로드시켜준다, 이걸로 새로고침되었을 때에도 게시판에서 초기값이 0이라 좋아요 리스트가 무조건 좋아요로 나오던 오류가 수정된다. */}
+        {isLoadUser && (
+          <Layout>
+            <Component {...pageProps} />{' '}
+          </Layout>
+        )}
       </ThemeProvider>
     </Provider>,
   );
