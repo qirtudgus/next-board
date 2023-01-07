f54ca11c78b885b0b4a3ef1d95eb06ed0880e70c
qirtudgus <qirtudgus@naver.com>
2023-01-07 11:00:45 +0900

    fix: 새로고침 시 좋아요버튼이 잘못 출력되던 오류 수정
    _app.tsx에서 유저정보를 디스패치 후 렌더링되도록 구조를 수정한것과,
    userInfoSlice에 idx값을 존재할 수 없는 '0'으로 변경
    그 외에 스타일링 추가

--- pages/posts/[idx].tsx
+++ pages/posts/[idx].tsx
@@ -3,7 +3,9 @@ import { GetServerSideProps } from 'next';
 import customAxios from '../../utils/customAxios';
 import { useAppSelector } from '../../store/store';
 import BasicButton from '../../components/BasicButton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
 
 interface Data {
   idx: number;
@@ -12,11 +14,51 @@ interface Data {
   date: string;
   userId: string;
   likeList: string;
+  viewCount: string;
 }
 
 export interface BoardInterface {
   data: Data[];
 }
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  padding: 15px 0 30px;
+  @media ${({ theme }) => theme.device.tablet} {
+    font-size: 1.7rem;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    font-size: 1.4rem;
+  }
+`;
+
+const UserInfo = styled.div`
+  padding-top: 50px;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  & p {
+    display: flex;
+    align-items: center;
+    padding-right: 1rem;
+  }
+  & svg {
+    opacity: 0.7;
+  }
+  & .userId {
+    font-size: 1.1rem;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    & .userId {
+      font-size: 1rem;
+    }
+    padding-top: 30px;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    padding-top: 20px;
+  }
+`;
 
 export default function Post({ data }: BoardInterface) {
   const router = useRouter();
@@ -26,14 +68,44 @@ export default function Post({ data }: BoardInterface) {
   const isLogin = useAppSelector((state) => state.userInfoSlice.isLogin);
   const [isLike, setIsLike] = useState(data[0].likeList.includes(userIdx.toString()));
 
+  useEffect(() => {
+    console.log(userIdx);
+    console.log(data[0].likeList.includes(userIdx.toString()));
+  }, []);
+
   return (
     <>
-      <h1>api 라우트로 받아온 포스트 데이터</h1>
-
-      <h3>{data[0].idx}</h3>
-      <h3>{data[0].title}</h3>
+      <Head>
+        <title>{data[0].title}</title>
+        <meta
+          property='og:title'
+          content={data[0].title}
+          key='title'
+        />
+      </Head>
+      <UserInfo>
+        <p className='userId'>{data[0].userId}</p>
+        <p>{data[0].date}</p>
+        <p>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            width='17px'
+            height='17px'
+            fill='#000000'
+          >
+            <path
+              d='M0 0h24v24H0V0z'
+              fill='none'
+            />
+            <path d='M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z' />
+          </svg>
+          {data[0].viewCount}
+        </p>
+      </UserInfo>
+      <Title>{data[0].title}</Title>
       <h3>{data[0].content}</h3>
-      <h3>{data[0].date}</h3>
+
       {userId === data[0].userId && (
         <>
           <BasicButton
@@ -58,49 +130,38 @@ export default function Post({ data }: BoardInterface) {
           ></BasicButton>
         </>
       )}
-      {isLogin ? (
-        isLike ? (
-          <button
-            onClick={() => {
-              console.log('좋아요 시도');
-              if (isLogin) {
-                customAxios('PUT', `/postlike?userIdx=${userIdx}&boardIdx=${idx}&behavior=unlike`)
-                  .then((res) => {
-                    console.log('좋아요 취소');
-                  })
-                  .then(() => setIsLike((prev) => !prev));
-              } else {
-                alert('로그인 후 이용 가능해요!');
-                return;
-              }
-            }}
-          >
-            좋아요 취소
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              if (isLogin) {
-                console.log('좋아요 시도');
-                customAxios('PUT', `/postlike?userIdx=${userIdx}&boardIdx=${idx}&behavior=like`)
-                  .then((res) => {
-                    console.log('좋아요 완료');
-                  })
-                  .then(() => setIsLike((prev) => !prev));
-              } else {
-                alert('로그인 후 이용 가능해요!');
-                return;
-              }
-            }}
-          >
-            좋아요
-          </button>
-        )
+      {isLike ? (
+        <button
+          onClick={() => {
+            console.log('좋아요 시도');
+            if (isLogin) {
+              customAxios('PUT', `/postlike?userIdx=${userIdx}&boardIdx=${idx}&behavior=unlike`)
+                .then((res) => {
+                  console.log('좋아요 취소');
+                })
+                .then(() => setIsLike((prev) => !prev));
+            } else {
+              alert('로그인 후 이용 가능해요!');
+              return;
+            }
+          }}
+        >
+          좋아요 취소
+        </button>
       ) : (
         <button
           onClick={() => {
-            alert('로그인 후 이용 가능해요!');
-            return;
+            if (isLogin) {
+              console.log('좋아요 시도');
+              customAxios('PUT', `/postlike?userIdx=${userIdx}&boardIdx=${idx}&behavior=like`)
+                .then((res) => {
+                  console.log('좋아요 완료');
+                })
+                .then(() => setIsLike((prev) => !prev));
+            } else {
+              alert('로그인 후 이용 가능해요!');
+              return;
+            }
           }}
         >
           좋아요
