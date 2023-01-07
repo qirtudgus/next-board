63788438be0fc5e3c9a82fd6c2813d1bb8ca837f
qirtudgus <qirtudgus@naver.com>
2023-01-07 18:48:40 +0900

    feat: SVG태그 너무 길어서 컴포넌트화

--- components/post.tsx
+++ components/post.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
+import CommentCountSVG from './CommentCountSVG';
+import LikeCountSVG from './LikeCountSVG';
+import ViewCountSVG from './viewCountSVG';
 
 const BoardLi = styled.li`
   &:first-child {
@@ -76,51 +79,15 @@ const Posts = ({ idx, userId, date, title, viewCount, commentCount, likeCount }:
           </BoardTitle>
           <BoardFooterInfo>
             <span>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='#000000'
-                width='17px'
-                height='17px'
-              >
-                <path
-                  d='M0 0h24v24H0V0z'
-                  fill='none'
-                />
-                <path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' />
-              </svg>
+              <LikeCountSVG />
               {likeCount}
-            </span>{' '}
+            </span>
             <span>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='#000000'
-                width='17px'
-                height='17px'
-              >
-                <path
-                  d='M0 0h24v24H0V0z'
-                  fill='none'
-                />
-                <path d='M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.17L4 17.17V4h16v12zM7 9h2v2H7zm8 0h2v2h-2zm-4 0h2v2h-2z' />
-              </svg>
+              <CommentCountSVG />
               {commentCount}
-            </span>{' '}
+            </span>
             <span>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                width='17px'
-                height='17px'
-                fill='#000000'
-              >
-                <path
-                  d='M0 0h24v24H0V0z'
-                  fill='none'
-                />
-                <path d='M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z' />
-              </svg>
+              <ViewCountSVG />
               {viewCount}
             </span>
           </BoardFooterInfo>
--- pages/posts/[idx].tsx
+++ pages/posts/[idx].tsx
@@ -6,6 +6,7 @@ import BasicButton from '../../components/BasicButton';
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
+import ViewCountSVG from '../../components/viewCountSVG';
 
 export interface BoardInterface {
   data: Data[];
@@ -79,11 +80,6 @@ export default function Post({ data }: BoardInterface) {
     likeCount: data[0].likeCount,
   });
 
-  useEffect(() => {
-    console.log(userIdx);
-    console.log(data[0].likeList.includes(userIdx.toString()));
-  }, []);
-
   return (
     <>
       <Head>
@@ -98,19 +94,7 @@ export default function Post({ data }: BoardInterface) {
         <p className='userId'>{post.userId}</p>
         <p>{post.date}</p>
         <p>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            width='17px'
-            height='17px'
-            fill='#000000'
-          >
-            <path
-              d='M0 0h24v24H0V0z'
-              fill='none'
-            />
-            <path d='M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z' />
-          </svg>
+          <ViewCountSVG />
           {post.viewCount}
         </p>
       </UserInfo>
