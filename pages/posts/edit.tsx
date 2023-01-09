10120b24e37a398ac52c3b9cc934c70b8c9e389f
qirtudgus <qirtudgus@naver.com>
2023-01-09 18:32:06 +0900

    fix : 타입 오류 수정

--- pages/posts/edit.tsx
+++ pages/posts/edit.tsx
@@ -4,20 +4,21 @@ import { RefObject, useEffect, useRef, useState } from 'react';
 import { SolidButton } from '../../components/BasicButton';
 import { useAppSelector } from '../../store/store';
 import customAxios from '../../utils/customAxios';
-import { PostInterface } from './[idx]';
+import { postPropsInterface } from './[idx]';
 
-interface EditInterface extends PostInterface {
+interface props {
+  data: postPropsInterface[];
   idx: string;
 }
 
-const Edit = (props: EditInterface) => {
+const Edit = ({ data, idx }: props) => {
   let titleRef = useRef() as RefObject<HTMLInputElement>;
   let contentRef = useRef() as RefObject<HTMLInputElement>;
   const userId = useAppSelector((state) => state.userInfoSlice.id);
   const router = useRouter();
 
   useEffect(() => {
-    if (userId !== props.data[0].userId) {
+    if (userId !== data[0].userId) {
       alert('권한이 없어요!');
       router.push('/');
     }
@@ -28,18 +29,18 @@ const Edit = (props: EditInterface) => {
       제목 :{' '}
       <input
         ref={titleRef}
-        defaultValue={props.data[0].title}
+        defaultValue={data[0].title}
       ></input>
       내용 :{' '}
       <input
         ref={contentRef}
-        defaultValue={props.data[0].content}
+        defaultValue={data[0].content}
       ></input>
       <SolidButton
         BasicButtonValue='수정하기'
         OnClick={() => {
           customAxios('PUT', '/posts', {
-            idx: props.idx,
+            idx: idx,
             title: titleRef.current?.value,
             content: contentRef.current?.value,
           }).then((res) => {
@@ -58,7 +59,7 @@ export default Edit;
 export const getServerSideProps: GetServerSideProps = async (context) => {
   //context 인자에는 다양한 키가 들어있다. 아래 코드는 context에서 동적 경로 페이지 정보를 가져와서 그 번호로 axios 요청을 한것
 
-  let { data }: EditInterface = await customAxios('GET', `/posts?idx=${context.query.idx}`);
+  let { data }: props = await customAxios('GET', `/posts?idx=${context.query.idx}`);
 
   //   console.log(context.query)
   // let { data }: BoardInterface = await axios.get(
