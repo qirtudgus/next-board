7d0fb2aa6224cc333504879691a51fbfe6a1d5ad
qirtudgus <qirtudgus@naver.com>
2023-01-09 18:19:58 +0900

    feat: 불필요 코드 제거

--- pages/intro.tsx
+++ pages/intro.tsx
@@ -1,27 +1,24 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import BasicTitle from '../components/BasicTitle';
 
-const Intro = ({ data, buildDate }: { data: string; buildDate: any }) => {
+//Intro.tsx
+export default function Intro({ data, buildDate }: { data: string; buildDate: any }) {
   return (
     <>
+      <Head>
+        <title>인트로</title>
+      </Head>
       <BasicTitle BasicTitleValue='안녕하세요?'></BasicTitle>
-      <p>이 곳은 Next의 getStaticProps를 통해</p>
-      <p>빌드타임에 데이터를 받아와 정적으로 생성된 페이지입니다.</p>
-
-      <p>revalidate에 false를 주었기때문에 아래 날짜는 다음 빌드때까지 캐시됩니다.</p>
-      <p>마지막 빌드 날짜입니다.</p>
       <p>{buildDate}</p>
     </>
   );
-};
-
-export default Intro;
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
   let day = new Date().toJSON();
   return {
     props: {
-      data: '겟스태틱프롭스',
       buildDate: day,
     }, // will be passed to the page component as props
     revalidate: false,
