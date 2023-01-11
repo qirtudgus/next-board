5a3b44409c98a3371ffac4cef8fa1272ef9f5d4e
qirtudgus <qirtudgus@naver.com>
2023-01-12 00:35:51 +0900

    feat: 이미지 슬라이드 추가
    props로 전달하는 이미지 갯수대로 늘어나도록 설계

---
+++ components/Swiper.tsx
@@ -0,0 +1,114 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Pagination, Navigation } from 'swiper';
+
+import styled from 'styled-components';
+import Image, { StaticImageData } from 'next/image';
+
+interface StyleInterface {
+  bulletBgColor: string;
+  bulletActiveColor: string;
+}
+
+const SwiperStyle = styled.div<StyleInterface>`
+  & .swiper-slide {
+    padding: 15px;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    & .swiper-slide {
+      padding: 10px;
+    }
+  }
+
+  & .swiper-pagination {
+    bottom: 0px;
+  }
+
+  & .swiper-pagination-bullet {
+    width: 20px;
+    height: 5px;
+    border-radius: 0px;
+    background: ${(props) => props.bulletBgColor};
+    opacity: 0.7;
+  }
+
+  & .swiper-pagination-bullet-active {
+    background: ${(props) => props.bulletActiveColor};
+    opacity: 1;
+  }
+
+  & .swiper-button-prev {
+    left: -3px;
+  }
+  & .swiper-button-next {
+    right: -3px;
+  }
+
+  & .swiper-button-prev,
+  .swiper-rtl,
+  .swiper-button-next {
+    z-index: 11;
+    color: ${(props) => props.bulletActiveColor};
+  }
+
+  & .swiper-button-next:after,
+  .swiper-button-prev:after {
+    font-size: 2rem !important;
+    font-weight: bold;
+  }
+
+  & img {
+    width: 100%;
+    height: 100%;
+    user-select: none;
+    pointer-events: none;
+    position: static !important;
+  }
+`;
+const Swipers = ({
+  imgArr,
+  bulletActiveColor,
+  bulletBgColor,
+}: {
+  imgArr: StaticImageData[];
+  bulletActiveColor: string;
+  bulletBgColor: string;
+}) => {
+  return (
+    <SwiperStyle
+      bulletActiveColor={bulletActiveColor}
+      bulletBgColor={bulletBgColor}
+    >
+      <Swiper
+        pagination={true}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {imgArr &&
+          (imgArr as StaticImageData[]).map((i, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <Image
+                  src={i}
+                  loading='eager'
+                  alt='image'
+                  fill
+                  quality={100}
+                ></Image>
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </SwiperStyle>
+  );
+};
+
+export default Swipers;
