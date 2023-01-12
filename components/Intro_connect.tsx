7045d0d69645b7ea97e0d19202ba138df58381a0
qirtudgus <qirtudgus@naver.com>
2023-01-13 00:58:46 +0900

    feat: 커넥트디브 추가

---
+++ components/Intro_connect.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useScroll } from 'framer-motion';
+
+const SectionWrap = styled.div`
+  height: 500px;
+  width: 100%;
+  background: #eee;
+`;
+
+const Intro_connect = () => {
+  return (
+    <>
+      <SectionWrap></SectionWrap>
+    </>
+  );
+};
+
+export default Intro_connect;
