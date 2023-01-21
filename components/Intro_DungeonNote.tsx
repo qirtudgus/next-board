e151fa5b90948f9a9037ddf1b70657c6a25f791c
qirtudgus <qirtudgus@naver.com>
2023-01-22 03:17:49 +0900

    feat: 스킬 라벨 추가

--- components/Intro_DungeonNote.tsx
+++ components/Intro_DungeonNote.tsx
@@ -6,12 +6,26 @@ import note0 from '../img/note_1200.webp';
 import note1 from '../img/note0_1200.webp';
 import note2 from '../img/note1_1200.webp';
 import note3 from '../img/note2_1200.webp';
-import { ContentName, ContentWrap, DescDiv, ImgDiv } from './Intro_Poraid';
+import { BottomBorder, ContentName, ContentWrap, DescDiv, ImgDiv, NewContentBox, StackList } from './Intro_Poraid';
 import styled from 'styled-components';
+import {
+  LabelSingle,
+  LabelTS,
+  LabelRedux,
+  LabelAWS,
+  LabelMysql,
+  LabelNode,
+  LabelReact,
+  LabelExpress,
+  LabelSC,
+  LabelGitActions,
+  LabelReduxSaga,
+  LabelFigma,
+} from './SVG_StackLabel';
 
 const ContentBox = styled(motion.div)`
   width: 90%;
-  max-width: 1300px;
+  max-width: 1500px;
   position: relative;
   align-items: center;
   justify-content: center;
@@ -19,7 +33,7 @@ const ContentBox = styled(motion.div)`
   border-radius: 20px;
   box-shadow: 0 0 70px 10px rgb(0 0 0 / 60%);
   background-color: #1d1d1f;
-  padding: 40px;
+  padding: 60px 20px;
   @media ${({ theme }) => theme.device.tablet} {
     padding: 15px;
     flex-direction: column;
@@ -57,23 +71,23 @@ const Intro_DungeonNote = () => {
   });
 
   //해당 함수를 통해 data attribute 세팅
-  useEffect(() => {
-    return scrollYProgress.onChange((latest) => {
-      if (ref.current) {
-        if (Number(scrollYProgress.get()) > 0.964) {
-          (document!.getElementById('header') as HTMLElement).classList.remove('text_black');
-        }
-        if (Number(scrollYProgress.get()) > 0 && Number(scrollYProgress.get()) < 0.96) {
-          (document!.getElementById('header') as HTMLElement).classList.add('text_black');
-        }
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   return scrollYProgress.onChange((latest) => {
+  //     if (ref.current) {
+  //       if (Number(scrollYProgress.get()) > 0.964) {
+  //         (document!.getElementById('header') as HTMLElement).classList.remove('text_black');
+  //       }
+  //       if (Number(scrollYProgress.get()) > 0 && Number(scrollYProgress.get()) < 0.96) {
+  //         (document!.getElementById('header') as HTMLElement).classList.add('text_black');
+  //       }
+  //     }
+  //   });
+  // }, []);
 
   return (
     <>
       <ContentWrap ref={ref}>
-        <ContentBox>
+        <NewContentBox>
           <DescDiv>
             <ContentName>
               <span>던전노트</span>
@@ -89,7 +103,7 @@ const Intro_DungeonNote = () => {
                       width='30px'
                       height='30px'
                       viewBox='0 0 24 24'
-                      fill='#fff'
+                      fill='#202124'
                     >
                       <path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z' />
                     </svg>
@@ -107,7 +121,7 @@ const Intro_DungeonNote = () => {
                       height='40px'
                       viewBox='0 0 24 24'
                       width='40px'
-                      fill='#fff'
+                      fill='#202124'
                     >
                       <g>
                         <rect
@@ -127,22 +141,46 @@ const Intro_DungeonNote = () => {
                 </a>
               </div>
             </ContentName>
+            <BottomBorder />
             추억의 공책게임을 RPG형식의 턴제 웹 게임으로 제작하였습니다.
+            <br /> - Redux와 Redux-saga를 통한 상태 관리
             <br />
-            - 레벨업을 통해 스텟과 스킬포인트를 모아 캐릭터를 강하게!
+            - jwt 토큰과 로컬스토리지를 이용한 로그인 구현
             <br />
-            - 미니게임과 던전을 탐험하여 돈과 경험치를 획득!
-            <br />- 내가 만났던 몬스터를 확인하는 도감 기능
-            <br />- 제일 높은 던전까지 탐험한 자는 누구? 랭킹 기능
+            - Redux를 통해 유저정보를 업데이트하고, MySql에 유저 정보를 관리.
+            <br />
+            - 유저중에 '던전 층수'가 높은 순으로 보여주는 Ranking 기능 구현
+            <br />- MySql 도감 테이블로 잡은 몬스터 기록을 안내해주는 페이지 구현
+            <br />- 애니메이션을 통해 Element에 등록되어있는 함수를 실행하는 기능 구현
+            <br />- LocalStorage를 사용하여 일회성 팝업 구현
+            <br />- react-intersection-observer를 사용해 무한 스크롤 구현
+            <br />- 네이버 클라우드 플랫폼에서 Node Express 서버 구축
+            <br />- LetsEncrypt를 사용하여 SSL 인증서 발급 후 https 적용
+            <br />- Git Actions을 통한 배포 및 빌드 자동화 구축
+            <BottomBorder />
+            <StackList>
+              <LabelSingle />
+              <LabelTS />
+              <LabelReact />
+              <LabelRedux />
+              <LabelReduxSaga />
+              <LabelSC />
+              <LabelAWS />
+              <LabelMysql />
+              <LabelNode />
+              <LabelExpress />
+              <LabelGitActions />
+              <LabelFigma />
+            </StackList>
           </DescDiv>
           <ImgDiv>
             <Swipers
               imgArr={[note0, note1, note2, note3]}
-              bulletBgColor='#8b8b8b'
-              bulletActiveColor='#fff'
+              bulletBgColor='#858585'
+              bulletActiveColor='#000'
             ></Swipers>
           </ImgDiv>
-        </ContentBox>
+        </NewContentBox>
       </ContentWrap>
     </>
   );
