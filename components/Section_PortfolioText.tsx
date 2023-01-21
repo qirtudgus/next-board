73a905b5c43e43ecc0e7138c30f56bfd2a94abe7
qirtudgus <qirtudgus@naver.com>
2023-01-22 03:18:23 +0900

    feat: 텍스트 추가

---
+++ components/Section_PortfolioText.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Text = styled.div`
+  width: 100%;
+  height: 200px;
+  font-weight: bold;
+  font-size: 72px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Section_PortfolioText = () => {
+  return <Text>Portfolio</Text>;
+};
+
+export default Section_PortfolioText;
