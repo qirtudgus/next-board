2836ad2438bf8048d476947b473b1caf27d1511c
qirtudgus <qirtudgus@naver.com>
2023-01-22 03:18:13 +0900

    feat: 스킬 라벨 추가

--- components/Intro_Poraid.tsx
+++ components/Intro_Poraid.tsx
@@ -6,23 +6,45 @@ import Swipers from './Swiper';
 import p0 from '../img/poraid0_1200.webp';
 import p1 from '../img/poraid1_1200.webp';
 import p2 from '../img/poraid2_1200.webp';
+import SVG_Card from './SVG_Card';
+import TsSVG from './svg/TsSVG';
+import SVG_StackLabel, {
+  LabelAWS,
+  LabelExpress,
+  LabelFigma,
+  LabelGitActions,
+  LabelMysql,
+  LabelNode,
+  LabelReact,
+  LabelRedux,
+  LabelReduxToolkit,
+  LabelSC,
+  LabelSingle,
+  LabelTS,
+} from './SVG_StackLabel';
+import ReduxSVG from './svg/ReduxSVG';
 
 export const ContentWrap = styled(motion.div)`
   width: 100%;
-  height: calc((100vh) * 1);
-  background: #1d1d1f;
-
+  /* height: calc((100vh) * 1); */
+  height: fit-content;
+  /* background: #1d1d1f; */
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   z-index: 10;
   position: relative;
+  @media ${({ theme }) => theme.device.tablet} {
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    /* height: 750px; */
+  }
 `;
 
 const ContentBox = styled(motion.div)`
   width: 90%;
-  max-width: 1300px;
+  max-width: 1500px;
   position: relative;
   align-items: center;
   justify-content: center;
@@ -30,7 +52,7 @@ const ContentBox = styled(motion.div)`
   border-radius: 20px;
   box-shadow: 0 0 70px 10px rgb(0 0 0 / 60%);
   background-color: #1d1d1f;
-  padding: 40px;
+  padding: 60px 20px;
   @media ${({ theme }) => theme.device.tablet} {
     padding: 15px;
     flex-direction: column;
@@ -60,25 +82,73 @@ const ContentBox = styled(motion.div)`
   }
 `;
 
+export const NewContentBox = styled(motion.div)`
+  width: 90%;
+  max-width: 1500px;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border-radius: 20px;
+  margin-bottom: 40px;
+  /* box-shadow: 0 0 10px 2px rgb(0 0 0 / 30%); */
+  /* background-color: #1d1d1f; */
+  padding: 40px 20px;
+  @media ${({ theme }) => theme.device.tablet} {
+    padding: 15px;
+    flex-direction: column;
+    /* box-shadow: 0 0 40px 7px rgb(0 0 0 / 60%); */
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    padding: 20px 15px;
+    flex-direction: column;
+    /* box-shadow: 0 0 15px 5px rgb(0 0 0 / 60%); */
+  }
+
+  /* &::before {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    border-radius: 20px;
+    width: calc(100% + 5px);
+    height: calc(100% + 5px);
+
+    background-image: linear-gradient(
+      var(--rotate),
+      rgba(241, 87, 87, 1) 0%,
+      rgba(249, 167, 78, 1) 45%,
+      rgba(255, 214, 0, 1) 100%
+    );
+    animation: spin 2.5s linear infinite;
+  } */
+`;
+
 export const DescDiv = styled.div`
-  width: 500px;
+  width: 600px;
+  margin-right: 40px;
+  /* min-height: 460px; */
   flex-direction: column;
   display: flex;
-  color: #fff;
+  /* color: #fff; */
 
   @media ${({ theme }) => theme.device.tablet} {
     width: 100%;
   }
   @media ${({ theme }) => theme.device.mobile} {
     width: 100%;
+    margin-right: 0px;
   }
+`;
+export const StackList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 export const ContentName = styled.div`
-  font-size: 2rem;
-  border-bottom: 1px solid#fff;
-  padding-bottom: 10px;
-  margin-bottom: 15px;
+  font-size: 50px;
+  /* border-bottom: 1px solid#cacaca; */
+  /* padding-bottom: 10px; */
+  /* margin-bottom: 15px; */
   font-weight: bold;
   display: flex;
 
@@ -90,26 +160,40 @@ export const ContentName = styled.div`
   }
 
   & span {
-    color: #fff;
+    /* color: #fff; */
     display: flex;
     align-items: center;
     margin-left: 5px;
   }
   & > div span svg:hover {
     cursor: pointer;
-    fill: #ef5a34;
+    /* fill: #ef5a34; */
+    fill: ${({ theme }) => theme.colors.main};
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    font-size: 44px;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    font-size: 34px;
   }
 `;
 
 export const ImgDiv = styled.div`
   display: block;
-  width: 60%;
+  width: 50%;
   @media ${({ theme }) => theme.device.tablet} {
     width: 75%;
   }
   @media ${({ theme }) => theme.device.mobile} {
     width: 100%;
   }
+`;
+
+export const BottomBorder = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #858585;
+  margin: 10px 0;
 `;
 
 const Intro_Poraid = () => {
@@ -120,22 +204,22 @@ const Intro_Poraid = () => {
   });
 
   //해당 함수를 통해 data attribute 세팅
-  useEffect(() => {
-    return scrollYProgress.onChange((latest) => {
-      if (ref.current) {
-        console.log(`포레이드 : ${Number(scrollYProgress.get())}`);
-        //0.479보다 크면 white 추가하기, 작으면 white 삭제하기
-        if (Number(scrollYProgress.get()) > 0.4789) {
-          (document!.getElementsByTagName('body') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.remove('white');
-          (document!.getElementsByTagName('header') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.remove('dark');
-        }
-        if (Number(scrollYProgress.get()) > 0 && Number(scrollYProgress.get()) < 0.4789) {
-          (document!.getElementsByTagName('body') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.add('white');
-          (document!.getElementsByTagName('header') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.add('dark');
-        }
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   return scrollYProgress.onChange((latest) => {
+  //     if (ref.current) {
+  //       console.log(`포레이드 : ${Number(scrollYProgress.get())}`);
+  //       //0.479보다 크면 white 추가하기, 작으면 white 삭제하기
+  //       if (Number(scrollYProgress.get()) > 0.4789) {
+  //         (document!.getElementsByTagName('body') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.remove('white');
+  //         (document!.getElementsByTagName('header') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.remove('dark');
+  //       }
+  //       if (Number(scrollYProgress.get()) > 0 && Number(scrollYProgress.get()) < 0.4789) {
+  //         (document!.getElementsByTagName('body') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.add('white');
+  //         (document!.getElementsByTagName('header') as HTMLCollectionOf<HTMLBodyElement>)[0].classList.add('dark');
+  //       }
+  //     }
+  //   });
+  // }, []);
 
   //height의 마이너스로 시작해 1로 당겨오는것..
   function useParallax(value: MotionValue<number>, distance: number) {
@@ -145,7 +229,7 @@ const Intro_Poraid = () => {
   return (
     <>
       <ContentWrap ref={ref}>
-        <ContentBox>
+        <NewContentBox>
           <DescDiv>
             <ContentName>
               <span>포레이드</span>
@@ -161,7 +245,7 @@ const Intro_Poraid = () => {
                       width='30px'
                       height='30px'
                       viewBox='0 0 24 24'
-                      fill='#ffffff'
+                      fill='#202124'
                     >
                       <path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z' />
                     </svg>
@@ -179,7 +263,7 @@ const Intro_Poraid = () => {
                       height='40px'
                       viewBox='0 0 24 24'
                       width='40px'
-                      fill='#ffffff'
+                      fill='#202124'
                     >
                       <g>
                         <rect
@@ -199,23 +283,41 @@ const Intro_Poraid = () => {
                 </a>
               </div>
             </ContentName>
-            닌텐도사의 포켓몬스터 게임 유저에게 도움을 줄 목적으로 제작한 사이트입니다. 실제로 서비스를 불특정다수에게
-            배포하였습니다.
+            <BottomBorder />
+            Socket.IO를 코어로 제작한 웹 사이트입니다.
+            <br /> - Redux-Toolkit를 통한 상태 관리
             <br />
-            - 사용자간 실시간 소통 및 접속자 수 안내
+            - Socket Session으로 사용자를 식별하여 접속자 수 안내
             <br />
-            - 기기별 알람 기능 구현
-            <br />- 주기별 서버 데이터 정리
-            <br />- 반응형 웹 구축
+            - Web Notification API와 Vibrate API를 통한 PC, mobile 진동 및 소리 알람 기능 구현
+            <br />- node schedule을 사용하여 주기별 서버 데이터 정리
+            <br />- 자신이 생성한 Socket Message를 자신만이 다시 삭제할 수 있는 기능 구현
+            <br />- AWS EC2 amazon-linux2 환경에서 서버 구축
+            <br />- LetsEncrypt를 사용하여 SSL 인증서 발급 후 https 적용
+            <br />- Git Actions을 통한 배포 및 빌드 자동화 구축
+            <BottomBorder />
+            <StackList>
+              <LabelSingle />
+              <LabelTS />
+              <LabelReact />
+              <LabelRedux />
+              <LabelReduxToolkit />
+              <LabelSC />
+              <LabelAWS />
+              <LabelMysql />
+              <LabelNode />
+              <LabelExpress />
+              <LabelGitActions />
+            </StackList>
           </DescDiv>
           <ImgDiv>
             <Swipers
               imgArr={[p0, p1, p2]}
               bulletActiveColor='#ef5a34'
-              bulletBgColor='#ffffff'
+              bulletBgColor='#858585'
             ></Swipers>
           </ImgDiv>
-        </ContentBox>
+        </NewContentBox>
       </ContentWrap>
     </>
   );
